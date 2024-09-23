f6e38411a67b322e84bb25251bb8679222000616
Luskinhaaa <134635596+Luskinhaaa@users.noreply.github.com>
2024-09-23 17:04:27 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 
+window.onload = function() {
+    setTimeout(function() {
+        document.getElementById('loader').style.display = 'none';
+        document.getElementById('content').style.display = 'block';
+    }, 2000); // 2000 ms = 2 seconds
+};
